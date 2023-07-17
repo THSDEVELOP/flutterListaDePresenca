fc500a0c4a2a517047940fd89370e8472ff58ae7
Tharsys Santos <109577143+THSDEVELOP@users.noreply.github.com>
2023-07-17 13:15:07 -0300

    Add files via upload

--- lib/utils/drawer.dart
+++ lib/utils/drawer.dart
@@ -2,18 +2,13 @@ import 'package:flutter/material.dart';
 
 import '../pages/dados_cadastrais.dart';
 
-class Drawer1 extends StatefulWidget {
+class Drawer1 extends StatelessWidget {
   const Drawer1({super.key});
 
   @override
-  State<Drawer1> createState() => _Drawer1State();
-}
-
-class _Drawer1State extends State<Drawer1> {
-  @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
-        body: Padding(
+    return Drawer(
+        child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -67,7 +62,6 @@ class _Drawer1State extends State<Drawer1> {
             ],
           ),
         ),
-      ),
     );
   }
 }
