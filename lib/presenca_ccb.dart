553f8917b1c0d063072ff862b6815ab64b115dab
Tharsys Santos <109577143+THSDEVELOP@users.noreply.github.com>
2023-07-17 12:51:38 -0300

    Add files via upload

--- lib/presenca_ccb.dart
+++ lib/presenca_ccb.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:listaccb/pages/dados_cadastrais.dart';
 import 'package:listaccb/pages/login_page.dart';
-import 'pages/home_page.dart';
+import 'pages/main_page.dart';
 class PresencaCCB extends StatelessWidget {
   const PresencaCCB({super.key});
 
@@ -12,7 +13,7 @@ class PresencaCCB extends StatelessWidget {
       theme: ThemeData(
         canvasColor: const Color.fromARGB(230, 255, 254, 254),
         primarySwatch: Colors.brown, textTheme: GoogleFonts.robotoTextTheme()),
-      home: const LoginPage(),
+      home: const MainPage(),
     );
   }
 }
